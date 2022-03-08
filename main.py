561d894ff6501a73e205733140244f896fee2e75
ljw <1205793031@qq.com>
2022-03-08 15:37:27 +0800

    完成，功能仅为连接串口并拼接数据发回给串口再由蓝牙发送到手机上

--- main.py
+++ main.py
@@ -26,8 +26,8 @@ class Read_data_1(QThread):
     def change_port(self, port):
         self.port = port
         self.is_open = True
-        # self.old = np.array(re.findall('\d+', str(self.port.readline()))).astype(int)
-        self.old = np.zeros(5, dtype=int)
+        self.old = np.array(re.findall('\d+', str(self.port.readline()))).astype(int)
+        # self.old = np.zeros(5, dtype=int)
 
     def run(self):
         while True:
@@ -40,15 +40,18 @@ class Read_data_1(QThread):
                 self.data = self.data.astype(int)
                 # print(self.data)
                 for i in range(5):
-                    if self.data[i] != self.old[i]:
-                        if not self.change[i]:
-                        # changevalue = self.data[i] - self.old[i]
-                            self.changeforce.emit(0, i, 1)
-                            self.change[i] = True
-                    else:
-                        if self.change[i]:
-                            self.changeforce.emit(0, i, 0)
-                            self.change[i] = False
+                    if self.data[i] - self.old[i] > 5:
+                        # if not self.change[i]:
+                        changevalue = self.data[i] - self.old[i]
+                        self.changeforce.emit(0, i, changevalue)
+                            # self.change[i] = True
+                            # print('5: ' + self.change)
+                    elif self.data[i] - self.old[i] < 5:
+                        # if self.change[i]:
+                        self.changeforce.emit(0, i, 0)
+                            # self.change[i] = False
+                            # print('5: ' + self.change)
+                QThread.msleep(50)
             pass
 
 class Read_data_2(QThread):
@@ -65,8 +68,8 @@ class Read_data_2(QThread):
     def change_port(self, port):
         self.port = port
         self.is_open = True
-        # self.old = np.array(re.findall('\d+', str(self.port.readline()))).astype(int)
-        self.old = np.zeros(5, dtype=int)
+        self.old = np.array(re.findall('\d+', str(self.port.readline()))).astype(int)
+        # self.old = np.zeros(5, dtype=int)
 
     def run(self):
         while True:
@@ -79,15 +82,18 @@ class Read_data_2(QThread):
                 self.data = self.data.astype(int)
                 # print(self.data)
                 for i in range(5):
-                    if self.data[i] != self.old[i]:
-                        if not self.change[i]:
-                        # changevalue = self.data[i] - self.old[i]
-                            self.changeforce.emit(1, i, 1)
-                            self.change[i] = True
-                    else:
-                        if self.change[i]:
-                            self.changeforce.emit(1, i, 0)
-                            self.change[i] = False
+                    if self.data[i] - self.old[i] > 3:
+                        # if not self.change[i]:
+                        changevalue = self.data[i] - self.old[i]
+                        self.changeforce.emit(1, i, changevalue)
+                            # self.change[i] = True
+                            # print('5: ' + self.change)
+                    elif self.data[i] - self.old[i] < 3:
+                        # if self.change[i]:
+                        self.changeforce.emit(1, i, 0)
+                            # self.change[i] = False
+                            # print('5: ' + self.change)
+                QThread.msleep(50)
             pass
 
 class Read_data_3(QThread):
@@ -104,8 +110,8 @@ class Read_data_3(QThread):
     def change_port(self, port):
         self.port = port
         self.is_open = True
-        # self.old = np.array(re.findall('\d+', str(self.port.readline()))).astype(int)
-        self.old = np.zeros(5, dtype=int)
+        self.old = np.array(re.findall('\d+', str(self.port.readline()))).astype(int)
+        # self.old = np.zeros(5, dtype=int)
 
     def run(self):
         while True:
@@ -118,15 +124,30 @@ class Read_data_3(QThread):
                 self.data = self.data.astype(int)
                 # print(self.data)
                 for i in range(5):
-                    if self.data[i] != self.old[i]:
-                        if not self.change[i]:
-                        # changevalue = self.data[i] - self.old[i]
-                            self.changeforce.emit(2, i, 1)
-                            self.change[i] = True
-                    else:
-                        if self.change[i]:
-                            self.changeforce.emit(2, i, 0)
-                            self.change[i] = True
+                    if  i == 1:
+                        if self.data[1] - self.old[1] > 1:
+                            changevalue = self.data[i] - self.old[i]
+                            self.changeforce.emit(2, 1, changevalue)
+                        else:
+                            self.changeforce.emit(2, 1, 0)
+                    if  i == 4:
+                        if self.data[4] - self.old[4] > 200:
+                            changevalue = self.data[i] - self.old[i]
+                            self.changeforce.emit(2, 4, changevalue)
+                        else:
+                            self.changeforce.emit(2, 4, 0)
+                    if self.data[i] - self.old[i] > 20:
+                        # if not self.change[i]:
+                        changevalue = self.data[i] - self.old[i]
+                        self.changeforce.emit(2, i, changevalue)
+                            # self.change[i] = True
+                            # print('5: ' + self.change)
+                    elif self.data[i] - self.old[i] < 20:
+                        # if self.change[i]:
+                        self.changeforce.emit(2, i, 0)
+                            # self.change[i] = False
+                            # print('5: ' + self.change)
+                QThread.msleep(50)
             pass
 
 class Read_data_4(QThread):
@@ -143,8 +164,8 @@ class Read_data_4(QThread):
     def change_port(self, port):
         self.port = port
         self.is_open = True
-        # self.old = np.array(re.findall('\d+', str(self.port.readline()))).astype(int)
-        self.old = np.zeros(5, dtype=int)
+        self.old = np.array(re.findall('\d+', str(self.port.readline()))).astype(int)
+        # self.old = np.zeros(5, dtype=int)
 
     def run(self):
         while True:
@@ -157,15 +178,18 @@ class Read_data_4(QThread):
                 self.data = self.data.astype(int)
                 # print(self.data)
                 for i in range(5):
-                    if self.data[i] != self.old[i]:
-                        if not self.change[i]:
-                        # changevalue = self.data[i] - self.old[i]
-                            self.changeforce.emit(3, i, 1)
-                            self.change[i] = True
-                    else:
-                        if self.change[i]:
-                            self.changeforce.emit(3, i, 0)
-                            self.change[i] = False
+                    if self.data[i] - self.old[i] > 15:
+                        # if not self.change[i]:
+                        changevalue = self.data[i] - self.old[i]
+                        self.changeforce.emit(3, i, changevalue)
+                            # self.change[i] = True
+                            # print('5: ' + self.change)
+                    elif self.data[i] - self.old[i] < 15:
+                        # if self.change[i]:
+                        self.changeforce.emit(3, i, 0)
+                            # self.change[i] = False
+                            # print('5: ' + self.change)
+                QThread.msleep(50)
             pass
 
 class Read_data_5(QThread):
@@ -182,8 +206,8 @@ class Read_data_5(QThread):
     def change_port(self, port):
         self.port = port
         self.is_open = True
-        # self.old = np.array(re.findall('\d+', str(self.port.readline()))).astype(int)
-        self.old = np.zeros(5, dtype=int)
+        self.old = np.array(re.findall('\d+', str(self.port.readline()))).astype(int)
+        # self.old = np.zeros(5, dtype=int)
 
     def run(self):
         while True:
@@ -196,15 +220,18 @@ class Read_data_5(QThread):
                 self.data = self.data.astype(int)
                 # print(self.data)
                 for i in range(5):
-                    if self.data[i] != self.old[i]:
-                        if not self.change[i]:
-                        # changevalue = self.data[i] - self.old[i]
-                            self.changeforce.emit(4, i, 1)
-                            self.change[i] = True
-                    else:
-                        if self.change[i]:
-                            self.changeforce.emit(4, i, 0)
-                            self.change[i] = False
+                    if self.data[i] - self.old[i] > 15:
+                        # if not self.change[i]:
+                        changevalue = self.data[i] - self.old[i]
+                        self.changeforce.emit(4, i, changevalue)
+                            # self.change[i] = True
+                            # print('5: ' + self.change)
+                    elif self.data[i] - self.old[i] < 15:
+                        # if self.change[i]:
+                        self.changeforce.emit(4, i, 0)
+                            # self.change[i] = False
+                            # print('5: ' + self.change)
+                QThread.msleep(50)
             pass
 
 class Find_port(QThread):
@@ -256,6 +283,7 @@ class Main_window(QMainWindow, UI_2.Ui_Dialog):
         self.thread5.changeforce.connect(self.form_data)
         # 数据
         self.data = np.zeros([5, 5])
+        self.old_dat = ''
         # 波特率列表设置
         self.bote_list = ['9600', '115200']
         # 填充复选框内容
@@ -264,6 +292,7 @@ class Main_window(QMainWindow, UI_2.Ui_Dialog):
         self.comboBox_6.addItems(self.bote_list)
         self.comboBox_8.addItems(self.bote_list)
         self.comboBox_10.addItems(self.bote_list)
+        self.comboBox_12.addItems(self.bote_list)
         # 用于存放处理后串口列表
         list1 = []
         # 获取串口列表
@@ -284,14 +313,16 @@ class Main_window(QMainWindow, UI_2.Ui_Dialog):
         self.comboBox_5.addItems(list1)
         self.comboBox_7.addItems(list1)
         self.comboBox_9.addItems(list1)
+        self.comboBox_11.addItems(list1)
         # 按钮点击事件
         self.pushButton.clicked.connect(self.port_connect)
         self.pushButton_2.clicked.connect(self.send_cs)
         self.pushButton_3.clicked.connect(self.clear_content)
         self.textBrowser.insertPlainText("测试")
+        self.t = QTimer()
+        self.t.timeout.connect(self.send_timer)
 
-    def send_cs(self):
-        box = QMessageBox()
+    def send_timer(self):
         dat = str(int(self.data[0][0])) + '-' + str(int(self.data[0][1])) + '-' + str(int(self.data[0][2])) + '-' + str(
             int(self.data[0][3])) + '-' + str(int(self.data[0][4])) + '-' + str(int(self.data[1][0])) + '-' + str(
             int(self.data[1][1])) + '-' + str(int(self.data[1][2])) + '-' + str(int(self.data[1][3])) + '-' + str(
@@ -301,13 +332,38 @@ class Main_window(QMainWindow, UI_2.Ui_Dialog):
             int(self.data[3][3])) + '-' + str(int(self.data[3][4])) + '-' + str(int(self.data[4][0])) + '-' + str(
             int(self.data[4][1])) + '-' + str(int(self.data[4][2])) + '-' + str(int(self.data[4][3])) + '-' + str(
             int(self.data[4][4]))
-        try:
-            self.port5.write(dat.encode())
-        except Exception as e:
-            box.setText('<h1>尚未连接！！</h1>')
+
+        if dat == self.old_dat:
+            pass
         else:
-            box.setText('<h1>发送成功！！</h1>')
-        box.exec_()
+            try:
+                self.port6.write(dat.encode())
+            except Exception as e:
+                pass
+            else:
+                self.textBrowser.insertPlainText("发送数据：\n" + dat + '\n')
+                self.old_dat = dat
+
+    def send_cs(self):
+        # box = QMessageBox()
+        dat = str(int(self.data[0][0])) + '-' + str(int(self.data[0][1])) + '-' + str(int(self.data[0][2])) + '-' + str(
+            int(self.data[0][3])) + '-' + str(int(self.data[0][4])) + '-' + str(int(self.data[1][0])) + '-' + str(
+            int(self.data[1][1])) + '-' + str(int(self.data[1][2])) + '-' + str(int(self.data[1][3])) + '-' + str(
+            int(self.data[1][4])) + '-' + str(int(self.data[2][0])) + '-' + str(int(self.data[2][1])) + '-' + str(
+            int(self.data[2][2])) + '-' + str(int(self.data[2][3])) + '-' + str(int(self.data[2][4])) + '-' + str(
+            int(self.data[3][0])) + '-' + str(int(self.data[3][1])) + '-' + str(int(self.data[3][2])) + '-' + str(
+            int(self.data[3][3])) + '-' + str(int(self.data[3][4])) + '-' + str(int(self.data[4][0])) + '-' + str(
+            int(self.data[4][1])) + '-' + str(int(self.data[4][2])) + '-' + str(int(self.data[4][3])) + '-' + str(
+            int(self.data[4][4]))
+        self.textBrowser.insertPlainText("发送数据：\n" + dat + '\n')
+        # try:
+        #     self.port6.write(dat.encode())
+        # except Exception as e:
+        #     box.setText('<h1>尚未连接！！</h1>')
+        # else:
+        #     box.setText('<h1>发送成功！！'+ dat + '</h1>')
+        # box.exec_()
+
 
 
     def clear_content(self):
@@ -325,6 +381,7 @@ class Main_window(QMainWindow, UI_2.Ui_Dialog):
         self.comboBox_5.clear()
         self.comboBox_7.clear()
         self.comboBox_9.clear()
+        self.comboBox_11.clear()
         # 用于存放处理后串口列表
         list1 = []
         # 获取串口列表
@@ -345,6 +402,7 @@ class Main_window(QMainWindow, UI_2.Ui_Dialog):
         self.comboBox_5.addItems(list1)
         self.comboBox_7.addItems(list1)
         self.comboBox_9.addItems(list1)
+        self.comboBox_11.addItems(list1)
 
     # 串口连接函数
     def port_connect(self):
@@ -355,6 +413,7 @@ class Main_window(QMainWindow, UI_2.Ui_Dialog):
             self.port3 = serial.Serial(self.comboBox_5.currentText(), self.comboBox_6.currentText())
             self.port4 = serial.Serial(self.comboBox_7.currentText(), self.comboBox_8.currentText())
             self.port5 = serial.Serial(self.comboBox_9.currentText(), self.comboBox_10.currentText())
+            self.port6 = serial.Serial(self.comboBox_11.currentText(), self.comboBox_12.currentText())
         except:
             box.setText("<h1>连接失败！！</h1>")
         else:
@@ -368,14 +427,15 @@ class Main_window(QMainWindow, UI_2.Ui_Dialog):
             self.thread3.start()
             self.thread4.start()
             self.thread5.start()
+            self.t.start(1000)
             box.setText("<h1>连接成功！！</h1>")
         box.exec_()
 
     def form_data(self, a, b, c):
         self.data[a][b] = c
-        dat = str(int(self.data[0][0])) + '-' + str(int(self.data[0][1])) + '-' + str(int(self.data[0][2])) + '-' + str(int(self.data[0][3])) + '-' + str(int(self.data[0][4])) + '-' + str(int(self.data[1][0])) + '-' + str(int(self.data[1][1])) + '-' + str(int(self.data[1][2])) + '-' + str(int(self.data[1][3])) + '-' + str(int(self.data[1][4])) + '-' + str(int(self.data[2][0])) + '-' + str(int(self.data[2][1])) + '-' + str(int(self.data[2][2])) + '-' + str(int(self.data[2][3])) + '-' + str(int(self.data[2][4])) + '-' + str(int(self.data[3][0])) + '-' + str(int(self.data[3][1])) + '-' + str(int(self.data[3][2])) + '-' + str(int(self.data[3][3])) + '-' + str(int(self.data[3][4])) + '-' + str(int(self.data[4][0])) + '-' + str(int(self.data[4][1])) + '-' + str(int(self.data[4][2])) + '-' + str(int(self.data[4][3])) + '-' + str(int(self.data[4][4]))
-        self.textBrowser.insertPlainText("发送数据：\n" + dat + '\n')
-        # self.port5.write(dat.encode())
+        # dat = str(int(self.data[0][0])) + '-' + str(int(self.data[0][1])) + '-' + str(int(self.data[0][2])) + '-' + str(int(self.data[0][3])) + '-' + str(int(self.data[0][4])) + '-' + str(int(self.data[1][0])) + '-' + str(int(self.data[1][1])) + '-' + str(int(self.data[1][2])) + '-' + str(int(self.data[1][3])) + '-' + str(int(self.data[1][4])) + '-' + str(int(self.data[2][0])) + '-' + str(int(self.data[2][1])) + '-' + str(int(self.data[2][2])) + '-' + str(int(self.data[2][3])) + '-' + str(int(self.data[2][4])) + '-' + str(int(self.data[3][0])) + '-' + str(int(self.data[3][1])) + '-' + str(int(self.data[3][2])) + '-' + str(int(self.data[3][3])) + '-' + str(int(self.data[3][4])) + '-' + str(int(self.data[4][0])) + '-' + str(int(self.data[4][1])) + '-' + str(int(self.data[4][2])) + '-' + str(int(self.data[4][3])) + '-' + str(int(self.data[4][4]))
+        # self.textBrowser.insertPlainText("发送数据：\n" + dat + '\n')
+        # self.port6.write(dat.encode())
 
 
 if __name__ == '__main__':
